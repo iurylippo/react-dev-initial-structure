179ecaef2c3057b8b8991731e70df7913fcf37c8
Iury Lippo <iuryvieira.lippo@gmail.com>
2020-12-23 10:43:40 -0300

    add: iframe googlechart

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Iframe from 'react-iframe';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { Container } from 'reactstrap';
@@ -15,6 +16,13 @@ const Main = () => {
     return (
         <Container>
             <h1>Dashboard</h1>
+            <Iframe
+                width="100%"
+                height="1000px"
+                url="https://datastudio.google.com/embed/reporting/23ad7fc0-b4c2-4459-a3b3-85b5ca1f19da/page/1M"
+                allowFullScreen
+                frameBorder={0}
+            />
         </Container>
     );
 };
